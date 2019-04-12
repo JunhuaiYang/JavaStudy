d02207aa4bb57bc25987d91516f0ca72e747bc00
Junhuai <850217160@QQ.com>
2019-04-12 19:59:12 +0800

    dfs

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -1,9 +1,13 @@
 package test;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class test {
 	public static void main(String[]args){
-		int [][][]x = new int[12][5][2];
-		System.out.println(x.length + " " + x[0].length + " " + x[0][0].length);
+//		int [][][]x = new int[12][5][2];
+//		System.out.println(x.length + " " + x[0].length + " " + x[0][0].length);
 //		F f = new F();
 //		System.out.println(f.i ); 
 //		System.out .println(f.s);
@@ -13,14 +17,22 @@ public class test {
 //		System.out.println(F.s); 
 //		F.imethod(); 
 //		F.smethod();
-	}
-	
-
+		try(
+			FileInputStream fis = new FileInputStream("test.dat");)
+		{}
+		catch(FileNotFoundException ex){ 
+			ex.printStackTrace();
+		}
+		catch(IOException ex){ 
+			ex.printStackTrace();
+		}
+		
+		}
 }
 
-class F { 
-	int i ; 
-	static String s = "!";
-	void imethod() { }
-	static void smethod() { }
-}
+//class F { 
+//	int i ; 
+//	static String s = "!";
+//	void imethod() { }
+//	static void smethod() { }
+//}
